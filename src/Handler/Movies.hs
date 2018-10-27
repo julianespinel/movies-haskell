ad8f68014e54787f7992b2faddc6eb255fdcf182
Julian Espinel <julianespinel@gmail.com>
2018-10-27 13:21:59 -0500

    Limit comment to col 80th.

--- src/Handler/Movies.hs
+++ src/Handler/Movies.hs
@@ -4,7 +4,8 @@ import Import
 
 postMoviesR :: Handler Value
 postMoviesR = do
-    -- requireJsonBody will parse the request body into the appropriate type, or return a 400 status code if the request JSON is invalid.
+    -- requireJsonBody will parse the request body into the appropriate type,
+    -- or return a 400 status code if the request JSON is invalid.
     -- (The ToJSON and FromJSON instances are derived in the config/models file).
     movie <- requireJsonBody :: Handler Movie
     insertedMovie <- runDB $ insertEntity movie
